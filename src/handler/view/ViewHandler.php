cd8a2cc0af65a4b9f9f0c4a8b3180c6cf2460f64
Elger van Boxtel <elgervb@gmail.com>
2015-12-17 15:17:20 +0100

    add view handler

---
+++ src/handler/view/ViewHandler.php
@@ -0,0 +1,41 @@
+<?php
+namespace handler\view;
+
+use handler\IHander;
+use http\HttpContext;
+
+/**
+ * Handler for views
+ *
+ * @author elger
+ */
+class ViewHandler implements IHander
+{
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \handler\IHander::handle()
+     */
+    public function handle($object)
+    {
+        // render the page
+        $responce = HttpContext::get()->getResponse();
+        $responce->setStatusCode(200);
+        $responce->setContentType('text/html');
+        $responce->getWriter()->write(is_string($object) ? $object : $object->render());
+        $responce->flush();
+        
+        return $object;
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \handler\IHander::accept()
+     */
+    public function accept($object)
+    {
+        return is_string($object) || (is_object($object) && is_subclass_of($object, '\view\IView'));
+    }
+}
